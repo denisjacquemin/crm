0ab3d08784c4f4dc6de9c113db7ee708a06d42b6
Denis Jacquemin <denis.jacquemin@gmail.com>
2022-11-13 13:58:00 +0100

    setting up i18next

--- index.js
+++ index.js
@@ -1,25 +1,63 @@
 require("dotenv").config();
+const i18next = require('i18next');
+const i18Middleware = require('i18next-http-middleware');
+const i18nBackend = require('i18next-fs-backend');
+const cookieParser = require('cookie-parser')
+
 const express = require("express");
 const path = require("path");
 const app = express();
 
+i18next.use(i18nBackend)
+       .use(i18Middleware.LanguageDetector)
+       .init({
+          detection: {
+            lookupCookie: 'lng',
+            caches: ['cookie']
+          },
+          backend: {
+              // eslint-disable-next-line no-path-concat
+              loadPath: __dirname + '/locales/{{lng}}/{{ns}}.json',
+              // eslint-disable-next-line no-path-concat
+              addPath: __dirname + '/locales/{{lng}}/{{ns}}.missing.json'
+            },
+            fallbackLng: 'en',
+            // nonExplicitSupportedLngs: true,
+            // supportedLngs: ['en', 'de'],
+            load: 'languageOnly',
+            saveMissing: true,
+            nonExplicitSupportedLngs: true
+       });
 
 const { create } = require("express-handlebars");
-
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(i18Middleware.handle(i18next));
+
+app.get('/lang', (req, res) => {
+  res.send(JSON.stringify({
+    'req.language': req.language,
+    'req.i18n.language': req.i18n.language,
+    'req.i18n.languages': req.i18n.languages,
+    'req.i18n.languages[0]': req.i18n.languages[0],
+    'req.t("home.title")': req.t('home.title')
+  }, null, 2))
+})
+
 const hbs = create({
   extname: ".hbs",
   helpers: {
     API_HOSTNAME: function () {
       return process.env.API_HOSTNAME;
-    },
+    }
   },
 });
 app.engine(".hbs", hbs.engine);
 app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "views"));
+
 
 app.use("/", require("./routes/global"));
 app.use("/", require("./routes/users"));
--- routes/global.js
+++ routes/global.js
@@ -7,4 +7,43 @@ router.get("/", (req, res) => {
   });
 });
 
+router.get("/preferences", (req, res) => {
+  res.render("preferences", {
+    API_URL: process.env.API_URL,
+  });
+});
+
+router.post("/preferences", async (req, res) => {
+
+  // if user is authenticated, save preferences in db
+
+  // const response = await fetch(process.env.API_URL + "/api/users/preferences", {
+  //     method: "POST",
+  //     headers: {
+  //         "Content-Type": "application/json",
+  //         "Accept": "application/json"
+  //     },
+  //     body: formDataJsonString,
+  // });
+  // if (response.ok) {
+  //     window.location.replace("/app");
+  // };
+  // if (!response.ok) {
+  //     const {
+  //         code,
+  //         message
+  //     } =
+  //     await response.json();
+  //     throw new Error(message);
+  // }
+
+  req.i18n.changeLanguage(req.body.lng);
+
+
+
+  res.render("preferences", {
+    API_URL: process.env.API_URL,
+  });
+});
+
 module.exports = router;
