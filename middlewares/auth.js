8393012a86e40f2863e8e7c3205b4fc16b65121c
Denis Jacquemin <denis.jacquemin@gmail.com>
2022-11-14 14:49:56 +0100

    improving signin page

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,6 @@ module.exports = async function (req, res, next) {
   // fetch AMIAuthenticated from api/auth/amiauthenticated
   // if it's true, continue to next middleware
   // if it's false, redirect to login page
-
   const cookies = req.cookies;
 
   await fetch(`${process.env.API_URL}/api/auth/amiauthenticated`)
